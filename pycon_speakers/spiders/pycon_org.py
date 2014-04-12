e4f320becf4857ccfae9fd976525dd651cc7dbb4
Daniel Graña <dangra@gmail.com>
2014-04-12 15:54:51 -0400

    Add 2007 speakers

--- pycon_speakers/spiders/pycon_org.py
+++ pycon_speakers/spiders/pycon_org.py
@@ -6,13 +6,17 @@ from scrapy.http import Request
 
 from pycon_speakers.loaders import SpeakerLoader
 
-ARCHIVE = {2008: '20081216074155',
-           2009: '20091223043735',
-           2010: '20101213081713'}
+ARCHIVE = {
+    2007: '20071001215135',
+    2008: '20081216074155',
+    2009: '20091223043735',
+    2010: '20101213081713',
+}
+
 
 class PyConSpider(Spider):
     name = 'us.pycon.org'
-    years = '2008,2009,2010,2011,2012,2013,2014'
+    years = '2007,2008,2009,2010,2011,2012,2013,2014'
 
     def start_requests(self):
         years = [int(x) for x in self.years.split(',')]
@@ -23,6 +27,10 @@ class PyConSpider(Spider):
                 yield Request(url.format(year), meta=meta)
             elif 2008 <= year <= 2010:
                 url = 'https://web.archive.org/web/{0}/http://us.pycon.org/{1}/conference/talks/'
+                yield Request(url.format(ARCHIVE[year], year), meta=meta,
+                              callback=self._parse_2010)
+            elif year == 2007:
+                url = 'https://web.archive.org/web/{0}/http://us.pycon.org/apps07/talks/'
                 yield Request(url.format(ARCHIVE[year], year), meta=meta,
                               callback=self._parse_2010)
 
