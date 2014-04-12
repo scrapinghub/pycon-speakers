bdb93b5979b6cfbbc72aeed1cc302dc180bf06b4
Daniel Graña <dangra@gmail.com>
2014-04-12 12:19:13 -0400

    add requirements.txt

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'project',
-    version      = '1.0',
-    packages     = find_packages(),
-    entry_points = {'scrapy': ['settings = pycon_speakers.settings']},
+    name='pycon2014-scraper',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={'scrapy': ['settings = pycon_speakers.settings']},
+    install_requires=['Scrapy'],
 )
