0cf68c2d4782787b17de9bca1c8901b05fa38bc3
Andrés Moreira <andres@andresmoreira.com>
2014-04-12 13:32:35 -0300

    add 'years' argument to the spider. grab data from 2011

--- pycon_speakers/spiders/pycon_org.py
+++ pycon_speakers/spiders/pycon_org.py
@@ -10,19 +10,22 @@ class PyConSpider(Spider):
     name = 'us.pycon.org'
     base_url = 'http://us.pycon.org/%s/schedule/talks/list/'
 
+    def __init__(self, *args, **kwargs):
+        self.years = kwargs.get('years', '').split(',')
+
     def start_requests(self):
-        for year in [2014, 2013, 2012, 2011]:
+        for year in self.years or [2014, 2013, 2012, 2011]:
             yield Request("https://us.pycon.org/%s/schedule/" % str(year), callback=self.follow_presentation_links, meta={'conf_year': year})
 
     def follow_presentation_links(self, response):
         sel = Selector(response)
-        for link in sel.xpath("//a[contains(@href, '/presentation/')]/@href").extract():
+        for link in sel.xpath("//a[contains(@href, '/presentation/') or contains(@href, '/presentations/')]/@href").extract():
             yield Request(urljoin(response.url, link), callback=self.scrape_speakers, meta=response.meta)
 
     def scrape_speakers(self, response):
         sel = Selector(response)
         conf_year = response.meta['conf_year']
-        speaker =  sel.xpath("//a[contains(@href, '/speaker/')]/text()").extract()[0]
+        speaker =  sel.xpath("//a[contains(@href, '/speaker/profile/')]/text()").extract()[0]
         yield Speaker(name=speaker, year=conf_year)
 
 
