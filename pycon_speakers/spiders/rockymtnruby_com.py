668c3209d1ef0a758a033ebc3227f64052de6856
Gayane <pgayane@gmail.com>
2014-04-14 13:57:53 -0400

    ruby conference 2013 workshops added

--- pycon_speakers/spiders/rockymtnruby_com.py
+++ pycon_speakers/spiders/rockymtnruby_com.py
@@ -17,6 +17,9 @@ class RmRubySpider(Spider):
             url = "http://rockymtnruby.com/{0}"
             yield Request(url.format(self.year), meta=meta,
                           callback=self._parse_2013)
+            workshop_url =  "http://rockymtnruby.com/{0}/workshops"
+            yield Request(workshop_url.format(self.year), meta=meta,
+                          callback=self._parse_workshop_2013)
 
     def _parse_2013(self, response):
         for section in Selector(response).xpath("//div[contains(@class,'speaker')]"):
@@ -24,3 +27,11 @@ class RmRubySpider(Spider):
             il.add_xpath('name', "./a[@class='name']")
             il.add_value('year', str(response.meta['year']))
             yield il.load_item()
+
+    def _parse_workshop_2013(self, response):
+        for section in Selector(response).xpath("//div[contains(@id,'workshop')]"):
+            for name in section.xpath(".//h2/text()").extract()[0].split(','):
+                il = SpeakerLoader(selector=section)
+                il.add_value('name', name)
+                il.add_value('year', str(response.meta['year']))
+                yield il.load_item()        
