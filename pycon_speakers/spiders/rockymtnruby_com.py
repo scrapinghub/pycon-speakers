781ab977704a045bdf99a58211a4bcaa3985f287
Gayane <pgayane@gmail.com>
2014-04-14 14:58:32 -0400

    spider for ruby conf 2012 done

--- pycon_speakers/spiders/rockymtnruby_com.py
+++ pycon_speakers/spiders/rockymtnruby_com.py
@@ -9,17 +9,37 @@ from pycon_speakers.loaders import SpeakerLoader
 
 class RmRubySpider(Spider):
     name = 'rockymtnruby.com'
-    year = 2013
-
+    
     def start_requests(self):
-        meta = {'year': self.year}
-        if self.year == 2013:
-            url = "http://rockymtnruby.com/{0}"
-            yield Request(url.format(self.year), meta=meta,
-                          callback=self._parse_2013)
-            workshop_url =  "http://rockymtnruby.com/{0}/workshops"
-            yield Request(workshop_url.format(self.year), meta=meta,
-                          callback=self._parse_workshop_2013)
+        scraping_dict = {2012: [("http://rockymtnruby.com/2012", self._parse_2012),
+                                ("http://rockymtnruby.com/{0}/workshop", self._parse_workshop_2012)
+                                ],
+                        2013: [("http://rockymtnruby.com/2013", self._parse_2013),
+                                ("http://rockymtnruby.com/{0}/workshops", self._parse_workshop_2013)
+                                ]
+                        }
+
+        for year in scraping_dict.keys():
+            meta = {'year': year}
+            for (url, callback) in scraping_dict[year]:
+                yield Request(url.format(year), meta=meta,
+                              callback=callback)
+
+
+    def _parse_2012(self, response):
+        for section in Selector(response).xpath("//div[contains(@class,'speaker')]"):
+            il = SpeakerLoader(selector=section)
+            il.add_xpath('name', "./a/p")
+            il.add_value('year', str(response.meta['year']))
+            yield il.load_item()
+
+    def _parse_workshop_2012(self, response):
+        for section in Selector(response).xpath("//div[contains(@class,'speaker')]"):
+            for name in section.xpath(".//p/text()").extract():
+                il = SpeakerLoader(selector=section)
+                il.add_value('name', name)
+                il.add_value('year', str(response.meta['year']))
+                yield il.load_item()  
 
     def _parse_2013(self, response):
         for section in Selector(response).xpath("//div[contains(@class,'speaker')]"):
@@ -30,8 +50,15 @@ class RmRubySpider(Spider):
 
     def _parse_workshop_2013(self, response):
         for section in Selector(response).xpath("//div[contains(@id,'workshop')]"):
-            for name in section.xpath(".//h2/text()").extract()[0].split(','):
+            names = section.xpath(".//h2/text()").extract()[0]
+            for name in self._split_names(names):
                 il = SpeakerLoader(selector=section)
                 il.add_value('name', name)
                 il.add_value('year', str(response.meta['year']))
                 yield il.load_item()        
+
+    def _split_names(self, names):
+        name_list = []
+        for name in names.split(','):
+            name_list.extend(name.split(' with '))
+        return name_list
